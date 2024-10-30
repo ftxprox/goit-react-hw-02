819c6eabe9621757163746f09ae4527aaa458800
HryhorievOA <ftxprox@gmail.com>
2024-10-30 20:35:46 +0200

    add ppc

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,65 @@
-import userData from "../src/userData.json";
-import Profile from "../src/components/Profile/Profile";
-import friends from "./friends.json";
-import FriendList from "../src/components/FriendList/FriendList";
-import transactions from "./Transaction.json";
-import TransactionHistory from "../src/components/TransactionHistory/TransactionHistory";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
+import { useState, useEffect } from "react";
 
+function App() {
+  const [feedbackCounts, setFeedbackCounts] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
 
-const App = () => {
+  const handleFeedback = (type) => {
+    setFeedbackCounts((prevCounts) => ({
+      ...prevCounts,
+      [type]: prevCounts[type] + 1
+    }));
+  };
+
+  const clearFeedback = () => {
+    setFeedbackCounts({
+      good: 0,
+      neutral: 0,
+      bad: 0
+    });
+  };
+
+  const calculateTotalFeedback = () => {
+    return feedbackCounts.good + feedbackCounts.neutral + feedbackCounts.bad;
+  };
+
+  const calculatePositiveFeedbackPercentage = () => {
+    const total = calculateTotalFeedback();
+    return total > 0 ? Math.round((feedbackCounts.good / total) * 100) : 0;
+  };
+
+  useEffect(() => {
+    const storedFeedback = localStorage.getItem("feedbackCounts");
+    if (storedFeedback) {
+      setFeedbackCounts(JSON.parse(storedFeedback));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("feedbackCounts", JSON.stringify(feedbackCounts));
+  }, [feedbackCounts]);
+
+  const totalFeedback = calculateTotalFeedback();
+  const positiveFeedbackPercentage = calculatePositiveFeedbackPercentage();
+
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <h1>Sip Happens Café</h1>
+      <p>We value your feedback. Please let us know about your experience.</p>
+      <Options leaveFeedback={handleFeedback} resetFeedback={clearFeedback} totalFeedback={totalFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedbackCounts} totalFeedback={totalFeedback} positiveFeedback={positiveFeedbackPercentage} />
+      ) : (
+        <Notification message="No feedback given yet." />
+      )}
     </>
   );
-};
-
+}
 
 export default App;
